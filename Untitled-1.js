c55b777f4b56cc4aa4bceea7133111998ff5cef8
ayalin2003 <ayalinroanjohn@gmail.com>
2024-09-12 11:38:08 +0700

    roan ungo

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+let value = 42; // This is the variable we are evaluating
+
+if (value > 100) {
+    console.log("Value is greater than 100.");
+} else if (value > 50) {
+    console.log("Value is greater than 50 but less than or equal to 100.");
+} else if (value > 30) {
+    console.log("Value is greater than 30 but less than or equal to 50.");
+} else if (value > 10) {
+    console.log("Value is greater than 10 but less than or equal to 30.");
+} else {
+    console.log("Value is 10 or less.");
+}
